381c88436530a0d0e46409d53e1e326091ba6f9d
zyj_8 <2864389Z@student.gla.ac.uk>
2023-10-09 19:39:09 +0800

    update GUI：LoginPage

---
+++ run.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from GUI import LoginPage
+
+def main():
+    tk = Tk()
+    root.title("Vehicle Sharing System")
+    loginPage.LoginPage(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
